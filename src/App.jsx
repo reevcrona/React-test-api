278bcb5d0ecc4c75f3e5d2e4da48293e2ad40e1d
reevcrona <89569507+reevcrona@users.noreply.github.com>
2024-12-04 19:46:32 +0100

    added some conditionall rendering and state that keeps track of player score lives left and if the game is over

--- src/App.jsx
+++ src/App.jsx
@@ -11,24 +11,27 @@ function App() {
   const [apiToken,setApiToken] = useState(null)
   const [quizElements,setQuizElements] = useState([]);
   const [questionIndex,setQuestionIndex] = useState(0);
-  const [questionActive,setQuestionActive] = useState(false);
+  const [gameIsActive,setGameIsActive] = useState(false);
+  const [roundIsOver,setRoundIsOver] = useState(false)
   const [correctAnswers,setCorrectAnswers] = useState(0);
+  const [playerLives,setPlayLives] = useState(3);
+  const [isGameOver,setIsGameOver] = useState(false);
   
-  useEffect(() => {
-    fetchApiToken()
-    
-  },[])
+    useEffect(() => {
+      fetchApiToken()
+      
+    },[])
 
-  useEffect(() => {
-    
-    if(questionActive){
-      renderQuizElements()
-    }
-  },[data],questionIndex)
+    useEffect(() => {
+      
+      if(gameIsActive){
+        renderQuizElements()
+      }
+    },[data])
 
-  useEffect(() => {
-    console.log(questionIndex)
-  },[questionIndex])
+    useEffect(() => {
+      console.log(questionIndex)
+    },[questionIndex])
   
   const updateQuestionIndex = () => {
     setQuestionIndex((prevIndex) => {
@@ -84,12 +87,20 @@ function App() {
     if(value === data[index].correctAnswer){
         console.log("correct answer")
         setCorrectAnswers((prevState) => prevState + 1);
-        
+        setRoundIsOver((prevState => !prevState))
         
     }else{
       console.log("Wrong answer")
-     
-
+      setRoundIsOver((prevState => !prevState))
+      setPlayLives((prevState) => {
+        if(prevState -1 === 0){
+          setIsGameOver(true)
+          return prevState - 1
+        }else{
+          return prevState - 1
+        }
+      })
+      
     }
     
   }
@@ -105,6 +116,8 @@ function App() {
             <button onClick={(e) => checkAnswer(e,index)} value={item.answers[2]}>{item.answers[2]}</button>
             <button onClick={(e) => checkAnswer(e,index)} value={item.answers[3]}>{item.answers[3]}</button>
           </div>
+
+          
         )
       }))
   }
@@ -121,16 +134,18 @@ function App() {
   return (
     
       <div className='main-container'>
-        <h2>{correctAnswers}</h2>
+        {isGameOver && <h1>GAME IS OVER</h1>}
+        <h2>CORRECT ANSWERS:{correctAnswers}</h2>
+        <h2>PLAYER LIVES :{playerLives}</h2>
         <button onClick={fetchApiData}>API QUESTION DATA</button>
         <button onClick={() => console.log(apiToken)}>Log Token</button>
         <button onClick={() => console.log(data)}>Log ARRAY</button>
-        <button onClick={() => {renderQuizElements(),setQuestionActive(true)}}>RENDER ELEMETNS</button>
+        <button onClick={() => {renderQuizElements(),setGameIsActive(true)}}>RENDER ELEMETNS</button>
         <button onClick={() => console.log(questionIndex)}>LOG QUESTON INDEX</button>
         <button onClick={updateQuestionIndex}>ADD 1 TO QUESTION INDEX</button>
 
-        {quizElements[questionIndex]}
-
+        {!isGameOver ? quizElements[questionIndex]: ""}
+        {roundIsOver && <button onClick={() => {updateQuestionIndex(),setRoundIsOver(false)}}>Next question</button>}
         
       </div>
         
